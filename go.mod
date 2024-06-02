0e87381f241871c2fde0d6f95cacb6dc0960ff97
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2024-06-02 12:04:53 +0700

    🎉 init:

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.22.3
 
 require (
 	github.com/go-logr/stdr v1.2.2
+	github.com/rs/zerolog v1.33.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.52.0
 	go.opentelemetry.io/otel v1.27.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.27.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.27.0
@@ -12,8 +14,11 @@ require (
 
 require (
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	go.opentelemetry.io/otel/metric v1.27.0 // indirect
 	go.opentelemetry.io/otel/trace v1.27.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"context"
-	"log"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
+	"time"
+
+	"github.com/rs/zerolog/log"
 
 	"github.com/jirawan-chuapradit/tracing-go/otel"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 func main() {
@@ -16,6 +21,42 @@ func main() {
 	defer stop()
 
 	otel.InitOpenTelemetry(ctx)
-	log.Println("hello")
+	port := "9000"
+	// Start HTTP server.
+	srv := &http.Server{
+		Addr:         ":" + port,
+		BaseContext:  func(_ net.Listener) context.Context { return ctx },
+		ReadTimeout:  time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      newHTTPHandler(),
+	}
+	srvErr := make(chan error, 1)
+	go func() {
+		srvErr <- srv.ListenAndServe()
+	}()
 
+	log.Debug().Msg("starting server at http://localhost:" + port)
+
+	// Wait for interruption.
+	select {
+	case err := <-srvErr:
+		// Error when starting HTTP server.
+		log.Fatal().Err(err)
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		stop()
+	}
+
+	// When Shutdown is called, ListenAndServe immediately returns ErrServerClosed.
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Fatal().Err(err)
+	}
+}
+
+func newHTTPHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	handler := otelhttp.NewHandler(mux, "/")
+	return handler
 }
